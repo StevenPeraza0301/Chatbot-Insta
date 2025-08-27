98c8f84fe7ffb9cfd5e515becf1cf54dda37d04b
Steven Peraza Jimenez <steven.gabriel.jimenez@gmail.com>
2025-08-27 12:51:57 -0600

    v

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Activando entorno virtual..."
-source env/bin/activate
-
-echo "Iniciando Gunicorn con configuración optimizada..."
-gunicorn -w 4 -b 0.0.0.0:5000 app:app --timeout 120
